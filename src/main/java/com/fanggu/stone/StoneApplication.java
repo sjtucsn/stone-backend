c940cde1ea0fbaf4ccf2054bf286510fd42974a9
sjtucsn <570979438@qq.com>
2020-02-21 16:18:08 +0800

    配置spring-session

--- src/main/java/com/fanggu/stone/StoneApplication.java
+++ src/main/java/com/fanggu/stone/StoneApplication.java
@@ -3,9 +3,11 @@ package com.fanggu.stone;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @MapperScan("com.fanggu.stone.dao")
+@EnableRedisHttpSession
 public class StoneApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/fanggu/stone/controller/UserController.java
+++ src/main/java/com/fanggu/stone/controller/UserController.java
@@ -2,12 +2,17 @@ package com.fanggu.stone.controller;
 
 import com.fanggu.stone.dao.UserMapper;
 import com.fanggu.stone.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/user")
@@ -15,8 +20,12 @@ public class UserController {
     @Resource
     private UserMapper userMapper;
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/index")
-    public List<User> userIndexAction() {
+    public List<User> userIndexAction(HttpSession httpSession, String id) {
+        httpSession.setAttribute(id, new Random().nextInt(100));
+        logger.error("id:{};message:{};lastAccessTime:{};maxInactiveInterval:{}", httpSession.getId(), httpSession.getAttribute(id), httpSession.getLastAccessedTime(), httpSession.getMaxInactiveInterval());
         return userMapper.selectAll();
     }
 }
